c8544478deb96ffd31ca834773d7b5d99ed31c9f
Matias Saavedra <msaavedra@edrans.com>
2021-01-15 23:54:59 -0300

    Initial commit

---
+++ servchk.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import argparse
+import datetime
+
+parser = argparse.ArgumentParser(description="Manages and checks DHCP, DNS and FTP services running on the server")
+
+parser.add_argument('-c', '--check', nargs='+', choices=['dhcp', 'ftp', 'dns'])
+parser.add_argument('-ss', '--start-service', nargs='+', choices=['dhcp', 'ftp', 'dns'])
+parser.add_argument('-st', '--stop-service', nargs='+', choices=['dhcp', 'ftp', 'dns'])
+parser.add_argument('-ci', '--check-interval', type=int)
+
+pargs = parser.parse_args()
+print(pargs)
+print(pargs.check)

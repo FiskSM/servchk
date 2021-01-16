b2dd1d5d077c8056138d2c693e827c61dd06f48f
Matias Saavedra <msaavedra@edrans.com>
2021-01-16 00:48:42 -0300

    Add basic command parsing

--- servchk.py
+++ servchk.py
@@ -2,6 +2,7 @@
 
 import argparse
 import datetime
+import subprocess
 
 parser = argparse.ArgumentParser(description="Manages and checks DHCP, DNS and FTP services running on the server")
 
@@ -11,5 +12,6 @@ parser.add_argument('-st', '--stop-service', nargs='+', choices=['dhcp', 'ftp', 
 parser.add_argument('-ci', '--check-interval', type=int)
 
 pargs = parser.parse_args()
-#print(pargs)
-print(pargs.check)
+
+for serv in pargs.check:
+    subprocess.call(['sudo', 'systemctl', 'status' serv])

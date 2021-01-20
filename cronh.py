98ccce7094d8c6b435130494c160e6bd46f27814
Matias Saavedra <msaavedra@edrans.com>
2021-01-20 12:03:10 -0300

    finally

--- cronh.py
+++ cronh.py
@@ -1,17 +1,21 @@
 import subprocess
+import pathlib
 
-def editCronTab(interval):
+def editCronTab(args):
+    args = ' '.join(args)
+    path = pathlib.Path(__file__).parent.absolute()
+
     with open('/tmp/crontmp', 'a+') as f:
-        print(f'*/{interval} * * * * /usr/bin/python3 servchk -a', file=f)
-    subprocess.run(['crontab', '/tmp/crontmp'])
+        print(f'*/5 * * * * /usr/bin/python3 {path}/servchk.py -s {args}', file=f)
+    subprocess.run(['sudo', 'crontab', '/tmp/crontmp'])
     subprocess.run(['rm', '/tmp/crontmp'])
 
-def updateCronTab(interval):
+def updateCronTab(args):
     try:
         subprocess.run(['crontab', '-l', '>', '/tmp/crontmp'])
     except:
         print("Crontab file not present, generating the file...\n")
         subprocess.run(['touch', '/tmp/crontmp'])
 
-    editCronTab(interval)
+    editCronTab(args)
 
--- servchk.py
+++ servchk.py
@@ -2,13 +2,18 @@
 
 import argparse
 import subprocess
-import cronh
+from cronh import *
+import json
+from pathlib import Path
 
 def cleanArgs(args):
     for k in list(args.keys()):
         if not args[k]:
             del args[k]
     return args
+
+curpath = Path(__file__).parent.absolute()
+services = json.load(open(f'{curpath}/services.json'))
 
 parser = argparse.ArgumentParser(description="Manages and checks DHCP, DNS and FTP services running on the server")
 
@@ -17,36 +22,33 @@ parser.add_argument('-s', '--start-service', nargs='+', choices=['dhcp', 'ftp', 
 parser.add_argument('-r', '--restart-service', nargs='+', choices=['dhcp', 'ftp', 'dns'])
 parser.add_argument('-p', '--stop-service', nargs='+', choices=['dhcp', 'ftp', 'dns'])
 parser.add_argument('-a', '--check-start', nargs='+', choices=['dhcp', 'ftp', 'dns'])
-parser.add_argument('-i', '--check-interval', nargs='?', type=int, default=5)
+parser.add_argument('-i', '--check-interval', nargs='+', choices=['dhcp', 'ftp', 'dns'])
 
 pargs = parser.parse_args()
-
 args = cleanArgs(vars(pargs))
 
 if 'check' in args:
     for v in args['check']:
-        subprocess.run(['systemctl', 'status', v])
+        subprocess.run(['systemctl', 'status', services[v]])
 
 if 'start_service' in args:
     for v in args['start_service']:
-        subprocess.run(['sudo', 'systemctl', 'start', v])
+        subprocess.run(['sudo', 'systemctl', 'start', services[v]])
 
 if 'restart_service' in args:
     for v in args['restart_service']:
-        subprocess.run(['sudo', 'systemctl', 'restart', v])
+        subprocess.run(['sudo', 'systemctl', 'restart', services[v]])
 
 if 'stop_service' in args:
     for v in args['stop_service']:
-        subprocess.run(['sudo', 'systemctl', 'stop', v])
+        subprocess.run(['sudo', 'systemctl', 'stop', services[v]])
 
 if 'check_start' in args:
     for v in args['check_start']:
-        st = subprocess.check_output(['systemctl', 'show', v, '--value', '-p', 'ActiveState'])
+        st = subprocess.check_output(['systemctl', 'show', services[v], '--value', '-p', 'ActiveState'])
         if st.decode('utf8') != 'active':
-            subprocess.run(['sudo', 'systemctl', 'start', v])
+            subprocess.run(['sudo', 'systemctl', 'start', services[v]])
 
 if 'check_interval' in args:
     editCronTab(args['check_interval'])
-
-
 
